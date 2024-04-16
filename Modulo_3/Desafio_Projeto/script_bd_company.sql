e578beeb0dcf92b66b4012900046487acd9345ad
Alex Laudiano <laudiano@gmail.com>
2024-04-16 02:14:03 -0300

    Update script_bd_company.sql

--- Modulo_3/Desafio_Projeto/script_bd_company.sql
+++ Modulo_3/Desafio_Projeto/script_bd_company.sql
@@ -8,7 +8,7 @@ select * from information_schema.table_constraints
 -- create domain D_num as int check(D_num> 0 and D_num< 21);
 
 CREATE TABLE employee(
-	Fname varchar(15) not null,
+    Fname varchar(15) not null,
     Minit char(1),
     Lname varchar(15) not null,
     Ssn char(9) not null, 
@@ -23,8 +23,8 @@ CREATE TABLE employee(
 );
 
 alter table employee 
-	add constraint fk_employee 
-	foreign key(Super_ssn) references employee(Ssn)
+    add constraint fk_employee 
+    foreign key(Super_ssn) references employee(Ssn)
     on delete set null
     on update cascade;
 
@@ -33,7 +33,7 @@ alter table employee modify Dno int not null default 1;
 desc employee;
 
 create table department(
-	Dname varchar(15) not null,
+    Dname varchar(15) not null,
     Dnumber int not null,
     Mgr_ssn char(9) not null,
     Mgr_start_date date, 
@@ -48,14 +48,14 @@ create table department(
 -- modificar uma constraint: drop e add
 alter table department drop  departament_ibfk_1;
 alter table department 
-		add constraint fk_dept foreign key(Mgr_ssn) references employee(Ssn)
+	add constraint fk_dept foreign key(Mgr_ssn) references employee(Ssn)
         on update cascade;
 
 desc department;
 
 create table dept_locations(
-	Dnumber int not null,
-	Dlocation varchar(15) not null,
+    Dnumber int not null,
+    Dlocation varchar(15) not null,
     constraint pk_dept_locations primary key (Dnumber, Dlocation),
     constraint fk_dept_locations foreign key (Dnumber) references department (Dnumber)
 );
@@ -63,13 +63,13 @@ create table dept_locations(
 alter table dept_locations drop fk_dept_locations;
 
 alter table dept_locations 
-	add constraint fk_dept_locations foreign key (Dnumber) references department(Dnumber)
-	on delete cascade
+    add constraint fk_dept_locations foreign key (Dnumber) references department(Dnumber)
+    on delete cascade
     on update cascade;
 
 create table project(
-	Pname varchar(15) not null,
-	Pnumber int not null,
+    Pname varchar(15) not null,
+    Pnumber int not null,
     Plocation varchar(15),
     Dnum int not null,
     primary key (Pnumber),
@@ -79,7 +79,7 @@ create table project(
 
 
 create table works_on(
-	Essn char(9) not null,
+    Essn char(9) not null,
     Pno int not null,
     Hours decimal(3,1) not null,
     primary key (Essn, Pno),
@@ -89,7 +89,7 @@ create table works_on(
 
 drop table dependent;
 create table dependent(
-	Essn char(9) not null,
+    Essn char(9) not null,
     Dependent_name varchar(15) not null,
     Sex char(1),
     Bdate date,
