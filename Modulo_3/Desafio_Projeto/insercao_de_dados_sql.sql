883fc0b7888992c60e830e9e9d9fabedd77acd6e
Alex Laudiano <laudiano@gmail.com>
2024-04-16 02:06:46 -0300

    Update insercao_de_dados_sql.sql

--- Modulo_3/Desafio_Projeto/insercao_de_dados_sql.sql
+++ Modulo_3/Desafio_Projeto/insercao_de_dados_sql.sql
@@ -1,7 +1,7 @@
 use azure_company;
 
 insert into employee values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
-					('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
+			    ('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
                             ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
                             ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
                             ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
